b7d9ea6efc0b4410f3af10fcb6c42f7ca9d7eca4
Max Fehmerling <fehmer@tellonym.me>
2019-04-14 19:27:21 +0200

    feat: remove jobs after stats collection

--- src/metricCollector.ts
+++ src/metricCollector.ts
@@ -5,9 +5,15 @@ import { Redis } from 'ioredis';
 import { register as globalRegister, Registry } from 'prom-client';
 
 import { logger as globalLogger } from './logger';
-import { getJobCompleteStats, getStats, makeGuages, QueueGauges } from './queueGauges';
+import {
+  getJobCompleteStats,
+  getStats,
+  makeGuages,
+  QueueGauges,
+} from './queueGauges';
 
-export interface BullOptions extends Pick<bull.QueueOptions, Exclude<keyof bull.QueueOptions, 'redis'>> {
+export interface BullOptions
+  extends Pick<bull.QueueOptions, Exclude<keyof bull.QueueOptions, 'redis'>> {
   redis?: string | bull.QueueOptions['redis'];
 }
 
@@ -18,7 +24,6 @@ export interface QueueData<T = unknown> {
 }
 
 export class MetricCollector {
-
   private readonly logger: Logger;
 
   private readonly queues: QueueData<unknown>[];
@@ -52,6 +57,7 @@ export class MetricCollector {
         return;
       }
       await getJobCompleteStats(queue.prefix, queue.name, job, this.guages);
+      await job.remove();
     } catch (err) {
       this.logger.error({ err, job: id }, 'unable to fetch completed job');
     }
@@ -66,24 +72,30 @@ export class MetricCollector {
   }
 
   public async updateAll(): Promise<void> {
-    const updatePromises = this.queues.map(q => getStats(q.prefix, q.name, q.queue, this.guages));
+    const updatePromises = this.queues.map(q =>
+      getStats(q.prefix, q.name, q.queue, this.guages),
+    );
     await Promise.all(updatePromises);
   }
 
   public async ping(): Promise<void> {
-    await Promise.all(this.queues.map(async q => {
-      const client: Redis = (q.queue as any).client;
-      await client.ping();
-    }));
+    await Promise.all(
+      this.queues.map(async q => {
+        const client: Redis = (q.queue as any).client;
+        await client.ping();
+      }),
+    );
   }
 
   public async close(): Promise<void> {
     for (const q of this.queues) {
       for (const l of this.myListeners) {
-        (q.queue as any as EventEmitter).removeListener('global:completed', l);
+        ((q.queue as any) as EventEmitter).removeListener(
+          'global:completed',
+          l,
+        );
       }
     }
     await Promise.all(this.queues.map(q => q.queue.close()));
   }
-
 }
